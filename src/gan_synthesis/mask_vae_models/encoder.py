a966df5c3b531a60b0fd2033f8db223c0d08f2c6
Zain-Mrza <zzmirzazz@aol.com>
2025-09-03 20:38:51 -0700

    added todo

--- src/gan_synthesis/mask_vae_models/encoder.py
+++ src/gan_synthesis/mask_vae_models/encoder.py
@@ -6,49 +6,19 @@ class Encoder(nn.Module):
     def __init__(self, in_channels=1, latent_dim=128):
         super().__init__()
         self.latent_dim = latent_dim
-
-        # self.encoder = nn.Sequential(
-        #     nn.Conv2d(
-        #         in_channels, 32, kernel_size=4, stride=2, padding=1, bias=False
-        #     ),  # 96x96 → 48x48
-        #     nn.GroupNorm(num_groups=8, num_channels=32),
-        #     nn.SiLU(),
-
-        #     nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding="same", bias=False),
-        #     nn.GroupNorm(num_groups=8, num_channels=32),
-        #     nn.SiLU(),
-
-        #     nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding="same", bias=False),
-        #     nn.GroupNorm(num_groups=8, num_channels=32),
-        #     nn.SiLU(),
-
-        #     nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, bias=False),  # 48x48 → 24x24
-        #     nn.GroupNorm(num_groups=8, num_channels=64),
-        #     nn.SiLU(),
-
-        #     nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding="same"),
-        #     nn.GroupNorm(num_groups=8, num_channels=64),
-        #     nn.SiLU(),
-
-        #     nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # 24x24 → 12x12
-        #     nn.SiLU(),
-
-        #     nn.Conv2d(128, 128, kernel_size=3, stride=1, padding='same'),  # 24x24 → 12x12
-        #     nn.SiLU(),
-        #)
-
+        
         self.encoder = nn.Sequential(
-            Down(in_channels, 32), # Spatial dim halved (96 -> 48)
+            Down(in_channels, 32),            # Spatial dim halved (96 -> 48)
             Same(32),
             Same(32),
-            Down(32, 64),          # Spatial dim halved (48 -> 24)
+            Down(32, 64),                     # Spatial dim halved (48 -> 24)
             Same(64, 64),
-            Down(64, 128, use_norm=False),         # Spatial dim halved (24 -> 12)
+            Down(64, 128, use_norm=False),    # Spatial dim halved (24 -> 12)
             Same(128, 128, use_norm=False)
         )
 
         self.flatten = nn.Flatten()
-        self.fc_mu = nn.Linear(int(128 * 12*12), latent_dim)
+        self.fc_mu = nn.Linear(int(128 * 12*12), latent_dim)        # TODO: Consider global pooling instead
         self.fc_logvar = nn.Linear(int(128 * 12*12), latent_dim)
 
     def forward(self, x):
